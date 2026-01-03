32d4658da504268a85c8aac10d672e793ec7da34
GeorgeFence <jiri.ploc@email.cz>
2026-01-03 18:20:43 +0100

    Working version :)

--- BOMToPDF/Form1.Designer.cs
+++ BOMToPDF/Form1.Designer.cs
@@ -28,20 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboName = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboID = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelectBOM = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numTimeout = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimeout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(561, 632);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboName);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.comboID);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnSelectBOM);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(702, 657);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "BOM";
+            // 
+            // comboName
+            // 
+            this.comboName.FormattingEnabled = true;
+            this.comboName.Location = new System.Drawing.Point(573, 101);
+            this.comboName.Name = "comboName";
+            this.comboName.Size = new System.Drawing.Size(121, 21);
+            this.comboName.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(573, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Part Name";
+            // 
+            // comboID
+            // 
+            this.comboID.FormattingEnabled = true;
+            this.comboID.Location = new System.Drawing.Point(573, 61);
+            this.comboID.Name = "comboID";
+            this.comboID.Size = new System.Drawing.Size(121, 21);
+            this.comboID.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(573, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Part ID";
+            // 
+            // btnSelectBOM
+            // 
+            this.btnSelectBOM.Location = new System.Drawing.Point(573, 19);
+            this.btnSelectBOM.Name = "btnSelectBOM";
+            this.btnSelectBOM.Size = new System.Drawing.Size(121, 23);
+            this.btnSelectBOM.TabIndex = 1;
+            this.btnSelectBOM.Text = "Select file";
+            this.btnSelectBOM.UseVisualStyleBackColor = true;
+            this.btnSelectBOM.Click += new System.EventHandler(this.btnSelectBOM_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numTimeout);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.webView21);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Location = new System.Drawing.Point(720, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(897, 657);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PCB";
+            // 
+            // numTimeout
+            // 
+            this.numTimeout.Location = new System.Drawing.Point(158, 22);
+            this.numTimeout.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numTimeout.Name = "numTimeout";
+            this.numTimeout.Size = new System.Drawing.Size(120, 20);
+            this.numTimeout.TabIndex = 3;
+            this.numTimeout.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Timeout(s): ";
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(6, 48);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(885, 603);
+            this.webView21.TabIndex = 1;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(6, 19);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save PDF";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1309, 646);
+            this.ClientSize = new System.Drawing.Size(1629, 681);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.Text = "BOM to PDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimeout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelectBOM;
+        private System.Windows.Forms.ComboBox comboName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.NumericUpDown numTimeout;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- BOMToPDF/Form1.cs
+++ BOMToPDF/Form1.cs
@@ -1,20 +1,412 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using HtmlAgilityPack;
+using iText.IO.Image;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Borders;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using Microsoft.Playwright;
+using Microsoft.Web.WebView2.Core;
+using SkiaSharp;
+using Svg.Skia;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
+using HorizontalAlignment = iText.Layout.Properties.HorizontalAlignment;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+using Image = iText.Layout.Element.Image;
+using Path = System.IO.Path;
+using Rectangle = System.Drawing.Rectangle;
+using Size = System.Drawing.Size;
+using SKSvg = Svg.Skia.SKSvg;
 
 namespace BOMToPDF
 {
     public partial class Form1 : Form
     {
+        private string ExcelPath;
+        private readonly HttpClient httpClient = new HttpClient();
+
         public Form1()
         {
             InitializeComponent();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void LoadBomAndFillCombos()
+        {
+            using OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel files (*.xlsx)|*.xlsx|CSV files (*.csv)|*.csv";
+
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
+
+            string path = ofd.FileName;
+            ExcelPath = path;
+            var headers = new List<string>();
+            var table = new DataTable();
+
+            using (var wb = new XLWorkbook(path))
+            {
+                var ws = wb.Worksheet(1);
+
+                foreach (var cell in ws.Row(1).CellsUsed())
+                {
+                    string header = cell.GetString();
+                    headers.Add(header);
+                    table.Columns.Add(header);
+                }
+
+                foreach (var row in ws.RowsUsed().Skip(1).Take(20))
+                {
+                    table.Rows.Add(
+                        headers.Select((h, i) => row.Cell(i + 1).GetString()).ToArray()
+                    );
+                }
+            }
+
+            dataGridView1.DataSource = table;
+
+            comboID.DataSource = headers.ToList();
+
+            var headers2 = headers.ToList();
+            headers2.Insert(0, "Name from LSCS");
+            comboName.DataSource = headers2;
+        }
+
+        private void btnSelectBOM_Click(object sender, EventArgs e)
+        {
+            LoadBomAndFillCombos();
+        }
+
+        private async Task<string?> GetLcscDescriptionAsync(string url)
+        {
+            try
+            {
+                string html = await httpClient.GetStringAsync(url);
+                var doc = new HtmlDocument();
+                doc.LoadHtml(html);
+
+                var trNode = doc.DocumentNode.SelectSingleNode("//tr[td[contains(normalize-space(.), 'Description')]]");
+                if (trNode == null) return null;
+
+                var spanNode = trNode.SelectSingleNode(".//span[contains(@class,'major2--text')]");
+                if (spanNode == null)
+                {
+                    var td = trNode.SelectSingleNode("./td[2]");
+                    return td?.InnerText.Trim();
+                }
+
+                return spanNode.InnerText.Trim();
+            }
+            catch (HttpRequestException)
+            {
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private async void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ExcelPath))
+            {
+                MessageBox.Show("Load BOM first!");
+                return;
+            }
+
+            bool useScraper = (comboName.SelectedItem?.ToString() ?? "") == "Name from LSCS";
+            string colLcsc = comboID.SelectedItem?.ToString() ?? "";
+            string colName = comboName.SelectedItem?.ToString() ?? "";
+
+            try
+            {
+                var items = await ParseBomItemsAsync(ExcelPath, colLcsc, colName, useScraper,true);
+                CreateLabelsPdf(items);
+                MessageBox.Show("PDF generated.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private async Task<List<BomItem>> ParseBomItemsAsync(
+    string excelPath,
+    string colLcsc,
+    string colName,
+    bool useLcscScraper,
+    bool useJlcFootprint)
+        {
+            List<BomItem> items = new();
+
+            using var wb = new XLWorkbook(excelPath);
+            var ws = wb.Worksheet(1);
+
+            var headers = ws.Row(1)
+                .CellsUsed()
+                .Select((c, i) => new { Name = c.GetString(), Index = i + 1 })
+                .ToList();
+
+            int idxOrThrow(string name)
+            {
+                var found = headers.FirstOrDefault(h => h.Name == name);
+                if (found == null) throw new Exception($"Header '{name}' not found in Excel.");
+                return found.Index;
+            }
+
+            int iLcsc = idxOrThrow(colLcsc);
+            int iName = -1;
+            if (!useLcscScraper)
+                iName = idxOrThrow(colName);
+
+            foreach (var row in ws.RowsUsed().Skip(1))
+            {
+                string lcsc = row.Cell(iLcsc).GetString().Trim();
+                if (string.IsNullOrWhiteSpace(lcsc))
+                    continue;
+
+                string name = "";
+                byte[]? footprint = null;
+
+                if (!useLcscScraper)
+                    name = row.Cell(iName).GetString().Trim();
+                else
+                {
+                    try
+                    {
+                        var scraped = await GetLcscDescriptionAsync($"https://www.lcsc.com/product-detail/{lcsc}.html");
+                        name = scraped ?? "HTTP ERROR";
+                    }
+                    catch { name = "HTTP ERROR"; }
+                }
+
+                if (useJlcFootprint)
+                {
+                    try
+                    {
+                        using Bitmap bmp = await GetJlcPcbFootprintAsync(lcsc,Int32.Parse(numTimeout.Value.ToString()) * 1000);
+                        using var ms = new MemoryStream();
+                        bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                        footprint = ms.ToArray();
+                    }
+                    catch
+                    {
+                        footprint = GenerateGenericFootprint();
+                    }
+                }
+                else
+                {
+                    footprint = GenerateGenericFootprint();
+                }
+
+                items.Add(new BomItem
+                {
+                    LcscId = lcsc,
+                    Name = name,
+                    FootprintBitmap = footprint
+                });
+            }
+
+            return items;
+        }
+
+        public async Task<Bitmap> GetJlcPcbFootprintAsync(string jlcCode,int timeout = 2000)
+        {
+            string url =
+                $"https://jlcpcb.com/user-center/lcsvg/svg.html?code={jlcCode}&detail=1";
+
+            var tcsLoaded = new TaskCompletionSource<bool>();
+
+            void Handler(object? s, CoreWebView2NavigationCompletedEventArgs e)
+            {
+                webView21.NavigationCompleted -= Handler;
+                tcsLoaded.SetResult(true);
+            }
+
+            webView21.NavigationCompleted += Handler;
+            webView21.Source = new Uri(url);
+
+            await tcsLoaded.Task;
+            await Task.Delay(timeout);
+
+            string js = @"
+(() => {
+    const g = document.querySelector('g[c_origin]');
+    if (!g) return null;
+
+    const rect = g.getBoundingClientRect();
+
+    return {
+        x: rect.x,
+        y: rect.y,
+        width: rect.width,
+        height: rect.height,
+        dpr: window.devicePixelRatio
+    };
+})();
+";
+
+            string result = await webView21.ExecuteScriptAsync(js);
+
+            if (result == "null")
+                throw new Exception("SVG group <g c_origin> not found");
+
+            var box = JsonSerializer.Deserialize<SvgBox>(result)
+                      ?? throw new Exception("Invalid JS result");
+
+            using var ms = new MemoryStream();
+            await webView21.CoreWebView2.CapturePreviewAsync(
+                CoreWebView2CapturePreviewImageFormat.Png, ms);
+
+            using var fullBmp = new Bitmap(ms);
+
+            float scale = box.dpr;
+
+            Rectangle crop = new Rectangle(
+                (int)(box.x * scale),
+                (int)(box.y * scale),
+                (int)(box.width * scale),
+                (int)(box.height * scale)
+            );
+
+            Bitmap cropped = fullBmp.Clone(crop, fullBmp.PixelFormat);
+            return cropped;
+        }
+
+        private void CreateLabelsPdf(List<BomItem> items)
+        {
+            using SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PDF file (*.pdf)|*.pdf";
+            sfd.DefaultExt = "pdf";
+            sfd.FileName = "LCSC_Labels.pdf";
+
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
+
+            string outputPath = sfd.FileName;
+
+            float labelWidth = MmToPt(34f);
+            float labelHeight = MmToPt(12.5f);
+
+            using (var fs = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+            {
+                PdfWriter writer = new PdfWriter(fs);
+                PdfDocument pdf = new PdfDocument(writer);
+                Document doc = new Document(pdf, PageSize.A4);
+                doc.SetMargins(20, 20, 20, 20);
+
+                float usableWidth = PageSize.A4.GetWidth() - doc.GetLeftMargin() - doc.GetRightMargin();
+                float usableHeight = PageSize.A4.GetHeight() - doc.GetTopMargin() - doc.GetBottomMargin();
+
+                int cols = Math.Max(1, (int)(usableWidth / labelWidth));
+                int rows = Math.Max(1, (int)(usableHeight / labelHeight));
+
+                Table table = new Table(cols);
+                table.SetWidth(UnitValue.CreatePointValue(cols * labelWidth));
+
+                int labelIndex = 0;
+
+                foreach (var item in items)
+                {
+                    Cell cell = new Cell()
+                        .SetWidth(labelWidth)
+                        .SetHeight(labelHeight)
+                        .SetPadding(2)
+                        .SetBorder(new SolidBorder(0.5f));
+
+                    float imgColWidth = labelWidth * 0.45f;
+                    float textColWidth = labelWidth - imgColWidth - 2;
+
+                    Table innerTable = new Table(new float[] { imgColWidth, textColWidth })
+                        .SetWidth(UnitValue.CreatePointValue(labelWidth - 2))
+                        .SetBorder(Border.NO_BORDER);
+
+                    Cell imgCell = new Cell().SetBorder(Border.NO_BORDER).SetVerticalAlignment(VerticalAlignment.MIDDLE);
+                    if (item.FootprintBitmap != null && item.FootprintBitmap.Length > 0)
+                    {
+                        var imgData = iText.IO.Image.ImageDataFactory.Create(item.FootprintBitmap);
+                        Image img = new Image(imgData)
+                            .ScaleToFit(imgColWidth, labelHeight - 4)
+                            .SetHorizontalAlignment(HorizontalAlignment.RIGHT);
+                        imgCell.Add(img);
+                    }
+                    innerTable.AddCell(imgCell);
+
+                    Cell textCell = new Cell().SetBorder(Border.NO_BORDER).SetVerticalAlignment(VerticalAlignment.MIDDLE);
+                    textCell.Add(new Paragraph(item.LcscId).SetFontSize(6).SetMargin(0));
+                    textCell.Add(new Paragraph(item.Name).SetFontSize(5).SetMargin(0));
+                    innerTable.AddCell(textCell);
+
+                    cell.Add(innerTable);
+                    table.AddCell(cell);
+                    labelIndex++;
+
+                    if (labelIndex % (cols * rows) == 0)
+                    {
+                        doc.Add(table);
+                        doc.Add(new AreaBreak());
+                        table = new Table(cols);
+                        table.SetWidth(UnitValue.CreatePointValue(cols * labelWidth));
+                    }
+                }
+
+                if (table.GetNumberOfRows() > 0)
+                    doc.Add(table);
+
+                doc.Close();
+            }
+        }
+
+
+        private byte[] GenerateGenericFootprint()
+        {
+            using Bitmap bmp = new Bitmap(200, 80);
+            using Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            g.DrawRectangle(Pens.Black, 5, 5, 190, 70);
+            g.DrawString("ERROR", new Font("Arial", 28, FontStyle.Bold), Brushes.Black, new PointF(60, 15));
+            using MemoryStream ms = new MemoryStream();
+            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            return ms.ToArray();
+        }
+
+        private float MmToPt(float mm)
+        {
+            return mm * 72f / 25.4f;
+        } 
+    }
+
+    class BomItem
+    {
+        public string LcscId { get; set; } = "";
+        public string Name { get; set; } = "";
+
+        public byte[] FootprintBitmap { get; set; } = Array.Empty<byte>();
+    }
+    public class SvgBox
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float width { get; set; }
+        public float height { get; set; }
+        public float dpr { get; set; }
     }
 }
